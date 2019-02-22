0ae346a647866d0725d5948e6b6576f7795408f2
大貘 <weihua.lwh@alibaba-inc.com>
2019-02-22 10:49:00 +0800

    add branch step1

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <ChildComponent 
+      :professional = "professional"
+      :name = "name"
+      @one.native = "triggerOne"
+      @two = "triggerTwo"
+    />
+  </div>
+</template>
+
+<script>
+import ChildComponent from './components/ChildComponent.vue'
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      professional:  '屌丝码农',
+      name:'大漠'
+    }
+  },
+  components: {
+    ChildComponent
+  },
+  methods: {
+    triggerOne () {
+      console.log('one')
+    },
+    triggerTwo () {
+      console.log('two')
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
